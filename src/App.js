f5655a56439d417584f93d891d4c942c9431a359
Robinkphilip <robinphilipp8018@gmail.com>
2022-06-12 17:41:20 +0530

    inital commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Navigation />
+      {/* <Logo/>
+      <ImageLinkFrom />
+      <FaceRecogintion /> */}
+    </div>
+  );
+}
+
+export default App;
