cea90d7dee10ca34c0712c610c65da52238b2393
Robinkphilip <robinphilipp8018@gmail.com>
2022-06-15 18:54:52 +0530

    Rank component added

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,9 @@
+const Rank = () => {
+  return (
+    <div>
+      <div className="white f3">{"robin,your current rank is....."}</div>
+      <div className="white f1">{"#3"}</div>
+    </div>
+  );
+};
+export default Rank;
