788e212d2303eb2fcb3ff9d402b73509401c1745
Robinkphilip <robinphilipp8018@gmail.com>
2022-06-27 18:57:23 +0530

    front end completed

--- src/components/Navigation /Navigation.js
+++ src/components/Navigation /Navigation.js
@@ -1,34 +1,25 @@
 import React from "react";
+
 const Navigation = ({ onRouteChange, isSignedIn }) => {
   if (isSignedIn) {
     return (
-      <nav
-        style={{
-          display: "flex",
-          justifyContent: "flex-end",
-        }}
-      >
+      <nav style={{ display: "flex", justifyContent: "flex-end" }}>
         <p
           onClick={() => onRouteChange("signout")}
           className="f3 link dim black underline pa3 pointer"
         >
-          Sign out
+          Sign Out
         </p>
       </nav>
     );
   } else {
     return (
-      <nav
-        style={{
-          display: "flex",
-          justifyContent: "flex-end",
-        }}
-      >
+      <nav style={{ display: "flex", justifyContent: "flex-end" }}>
         <p
           onClick={() => onRouteChange("signin")}
           className="f3 link dim black underline pa3 pointer"
         >
-          Sign in
+          Sign In
         </p>
         <p
           onClick={() => onRouteChange("register")}
@@ -40,4 +31,5 @@ const Navigation = ({ onRouteChange, isSignedIn }) => {
     );
   }
 };
+
 export default Navigation;
